d8ee8629f8dd71aa4918bb02aa631fc6b831a181
Shasha Yan <shashayan@gmail.com>
2021-06-22 16:18:23 -0500

    commit tree 2424637918414cd684e6be49d347dcd33fad007c

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = ""
+const animal = "dog"
 
 function myAnimal() {
   return animal
@@ -8,12 +8,14 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  let animal = 'cat'
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  two = 2
+  
 }
